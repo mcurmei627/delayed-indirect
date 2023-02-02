00f8171f1f463effc1cae31c205cdeefe8163efd
Paris Zhang <pariszhang11@outlook.com>
2023-02-01 16:51:12 -0800

    add code to filter existing settings

--- exp_robustness.py
+++ exp_robustness.py
@@ -11,7 +11,7 @@ num_group = 2
 record_time = range(0, 400, 50)
 
 # mu, N, sigma are list of lists, each inner list contains settings for different groups
-mu = [[np.array([0,1]), np.array([1,0])]]
+mu = [[[0, 1], [1, 0]]]
 N = [[50, 50]]
 sigma = [[0.05, 0.05]]
 a = [2]
@@ -27,8 +27,8 @@ edge_removal = [False]
 
 is_ab_test = [True]
 treatment_probability = [0.1]
-# treatment_size = [None]
-# treatment_time = [None]
+treatment_size = [None]
+treatment_time = [50]
 
 metric_names = ['added_nodes', 'nodes', 'edges', 'avg_degree', 'degree_variance', 'bi_frac', 'global_clustering', 'avg_age', 'avg_nn', 'gini_coeff', 'num_rec', 'num_acc']
 for name in ['phase0', 'phase1', 'phase2_unmediated', 'phase2_mediated', 'phase3']:
@@ -44,7 +44,7 @@ for i in range(num_group):
     metric_names.append(f"gini_coeff_{i}")
 
 # for csv saving
-# setting_names = ['mu', 'N', 'sigma', 'num_group', 'a', 'b', 'acc_prob', 'beta', 'rec_how', 'acc_how', 'intervention_end', 'node_removal', 'edge_removal', 'is_ab_test', 'treatment_probability', 'treatment_size', 'treatment_time']
+# setting_names = ['mu', 'N', 'sigma', 'a', 'b', 'acc_prob', 'beta', 'rec_how', 'acc_how', 'intervention_end', 'node_removal', 'edge_removal', 'is_ab_test', 'treatment_probability', 'treatment_size', 'treatment_time']
 
 # metrics = []
 # for metric_name in metric_names:
@@ -73,7 +73,7 @@ def run(mu, N, sigma, a, b, acc_prob, beta, rec_how, acc_how, intervention_end=N
         acc_prob = 30
     
     conclusion = Conclusion(list(range(5)))
-    grp_info = [{'mean': mu[i], 'variance': sigma[i]*np.identity(2), 'size':N[i]} for i in range(len(N))]
+    grp_info = [{'mean': [np.array(m) for m in mu[i]], 'variance': sigma[i]*np.identity(2), 'size':N[i]} for i in range(len(N))]
 
     conclusion.run_experiments(grp_info, plot=False, init_how='embedding', Ns=sum(N), Nf=10, 
                             node_step=5, total_step=400, acc_how=acc_how,acc_prob=acc_prob, a=a, b=b,
@@ -87,12 +87,9 @@ def run(mu, N, sigma, a, b, acc_prob, beta, rec_how, acc_how, intervention_end=N
                             freq=5, record_each_run=False, rec_sample_fraction=0.1,
                             is_ab_test=is_ab_test, treatment_probability=treatment_probability, treatment_size=treatment_size, treatment_time=treatment_time)
     conclusion.experiments = None
-    
-    if treatment_size == None:
-        treatment_size = 'na'
 
     # for csv saving
-    # settings = [mu, N, sigma, num_group, a, b, acc_prob, beta, rec_how, acc_how, intervention_end, node_removal, edge_removal, is_ab_test, treatment_probability, treatment_size, treatment_time]
+    # settings = [mu, N, sigma, a, b, acc_prob, beta, rec_how, acc_how, intervention_end, node_removal, edge_removal, is_ab_test, treatment_probability, treatment_size, treatment_time]
     # metrics = []
     # for metric in metric_names:
     #     for t in record_time:
@@ -102,11 +99,10 @@ def run(mu, N, sigma, a, b, acc_prob, beta, rec_how, acc_how, intervention_end=N
     # with open(file_name, 'a') as f:
     #     writer = csv.writer(f)
     #     writer.writerow(row)
-    
 
     # for json saving
-    settings = {'mu':[m.tolist() for m in mu], 'N':N, 'sigma':sigma, 'num_group':num_group, 'a':a, 'b':b, 'acc_prob':acc_prob, 'beta':beta, 'rec_how':rec_how, 'acc_how':acc_how, 'intervention_end':intervention_end, 'node_removal':node_removal, 'edge_removal':edge_removal, 'is_ab_test':is_ab_test, 'treatment_probability':treatment_probability, 'treatment_size':treatment_size, 'treatment_time':treatment_time}
-    
+    settings = [mu, N, sigma, a, b, acc_prob, beta, rec_how, acc_how, intervention_end, node_removal, edge_removal, is_ab_test, treatment_probability, treatment_size, treatment_time]
+
     metrics = {}
     for metric in metric_names:
         metrics[metric] = {'avg':[], 'ci':[]}
@@ -126,6 +122,13 @@ def run(mu, N, sigma, a, b, acc_prob, beta, rec_how, acc_how, intervention_end=N
         with open(file_name, mode='w') as f:
             json.dump(feeds, f)
 
-settings = list(product(mu, N, sigma, a, b, acc_prob, beta, rec_how, acc_how, intervention_end, node_removal, edge_removal, is_ab_test, treatment_probability))
+settings = list(product(mu, N, sigma, a, b, acc_prob, beta, rec_how, acc_how, intervention_end, node_removal, edge_removal, is_ab_test, treatment_probability, treatment_size, treatment_time))
 
+# filter out existing settings
+if os.path.exists(file_name):
+    with open(file_name) as feedsjson:
+            existing_experiments = json.load(feedsjson)
+            existing_settings = [exp['settings'] for exp in existing_experiments]
+            print(existing_settings[0])
+            settings = [s for s in settings if list(s) not in existing_settings]
 Parallel(n_jobs=32)(delayed(run)(*setting) for setting in tqdm(settings))
