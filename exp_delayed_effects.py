93c01c5833c4c5a9ef63b54ea18c5848e1847d4d
Paris Zhang <pariszhang11@outlook.com>
2023-02-05 10:39:29 -0800

    add experiment code

--- exp_delayed_effects.py
+++ exp_delayed_effects.py
@@ -17,12 +17,14 @@ b = 5
 acc_prob=0.5
 beta=10
 
-def run(rec_how, acc_how, intervention_end=None, node_removal=False, edge_removal=False):
+def run(rec_how, acc_how, intervention_start=50, intervention_end=None, node_removal=False, edge_removal=False, treatment_probability=0.5):
     if intervention_end == None:
         intervention_time = []
         intervention_end = 'na'
     else:
-        intervention_time = list(range(50, intervention_end))
+        if intervention_start > intervention_end:
+            return
+        intervention_time = list(range(intervention_start, intervention_end))
         
     if acc_how == 'constant':
         acc_prob = 0.5
@@ -41,20 +43,26 @@ def run(rec_how, acc_how, intervention_end=None, node_removal=False, edge_remova
                             rec_how=rec_how,
                             node_removal=node_removal,
                             edge_removal=edge_removal,
-                            freq=5, record_each_run=False, rec_sample_fraction=0.1)
+                            freq=5, record_each_run=False, rec_sample_fraction=0.1,
+                            is_ab_test=True, treatment_probability=treatment_probability, treatment_time=intervention_start,
+                            is_static=True)
     conclusion.experiments = None
-    folder_name = 'experiments/exp_delayed_effects'
+    folder_name = 'experiments/exp_delayed_effects_static'
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
-    fname = f'{folder_name}/{rec_how}_{acc_how}_{intervention_end}_{node_removal}_{edge_removal}.pkl'
+    if isinstance(treatment_probability, list):
+        treatment_probability = f'group{treatment_probability[0]}'
+    fname = f'{folder_name}/{treatment_probability}_{rec_how}_{acc_how}_{intervention_start}_{intervention_end}_{node_removal}_{edge_removal}.pkl'
     with open(fname, 'wb') as f:
         pkl.dump([rec_how, acc_how, intervention_end, conclusion], f)
         
 rec_how  = ['embedding', 'random_fof']
 acc_how = ['constant', 'embedding']
-intervention_end = list(range(50, 401, 25))
-node_removal = [True, False]
+intervention_start = [50, 100]
+intervention_end = [50, 100, 200, 400]
+treatment_probability = [0.5, [1]]
+node_removal = [False]
 edge_removal = [True, False]
 
-settings = list(product(rec_how, acc_how, intervention_end, node_removal, edge_removal))
-Parallel(n_jobs=32)(delayed(run)(*setting) for setting in tqdm(settings))
+settings = list(product(rec_how, acc_how, intervention_start, intervention_end, node_removal, edge_removal, treatment_probability))
+Parallel(n_jobs=8)(delayed(run)(*setting) for setting in tqdm(settings))
