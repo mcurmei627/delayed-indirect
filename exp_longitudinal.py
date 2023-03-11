f83fbfc91807412e631f1e4fb66a43e10752b059
Mihaela <curmeimihaela@gmail.com>
2023-03-10 16:18:40 -0800

    Plot cleanup

--- exp_longitudinal.py
+++ exp_longitudinal.py
@@ -11,6 +11,21 @@ b = 5
 beta=10
 
 def run(setting, rec_how, acc_how, intervention_end=None, node_removal=False, edge_removal=False, p2_mediated=False):
+    folder_name = 'experiments/longitudinal'
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+    fname = f"{folder_name}/{setting}_{rec_how}_{acc_how}_{intervention_end}_{node_removal}_{edge_removal}_{p2_mediated}.pkl"
+    if os.path.exists(fname):
+        return
+    
+    if setting == 'baseline':
+        mu1 = np.array([0,1])
+        mu2 = np.array([1,0])
+        N1 = 50
+        N2 = 50
+        sigma1 = 0.05
+        sigma2 = 0.05
+        
     if setting == 'heterogeneity':
         mu1 = np.array([0,1.1])
         mu2 = np.array([1.1,0])
@@ -78,23 +93,18 @@ def run(setting, rec_how, acc_how, intervention_end=None, node_removal=False, ed
                             comp_grp_metrics=True,
                             freq=5, record_each_run=False, rec_sample_fraction=0.1)
     conclusion.experiments = None
-    folder_name = 'experiments/exp_group_effects'
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
-    fname = f"{folder_name}/{setting}_{rec_how}_{acc_how}_{intervention_end}_{node_removal}_{edge_removal}_{p2_mediated}.pkl"
+
     with open(fname, 'wb') as f:
         pkl.dump([rec_how, acc_how, intervention_end, conclusion], f)
 
-settings = ['heterogeneity', 'homogeneity', 'minority_homophily', 'majority_heterophily']   
-settings = ['homo+hetero']     
+settings = ['baseline', 'heterogeneity', 'homogeneity', 'minority_homophily', 'majority_heterophily', 'homo+hetero']       
 rec_how  = ['embedding', 'random_fof']
 acc_how = ['constant', 'embedding']
-intervention_end = list(range(100, 401, 100))
-intervention_end.append(50)
+intervention_end = [50, 100, 200, 300, 400]
 node_removal = [True, False]
 edge_removal = [True, False]
 p2_mediated = [True, False]
 
 settings = list(product(settings, rec_how, acc_how, intervention_end, node_removal, edge_removal, p2_mediated))
-Parallel(n_jobs=64)(delayed(run)(*setting) for setting in tqdm(settings))
+Parallel(n_jobs=-1)(delayed(run)(*setting) for setting in tqdm(settings))
     
